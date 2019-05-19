adb44a69e599241d1fde8234e3b87acb1a9e19e8
Eldar <mikailovek@gmail.com>
2019-05-19 12:49:37 +0300

    Это Наш первый комммит

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict';
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+
+
+// let persone = {
+// name: "John",
+// age: 29,
+// isMarried: false
+//   };
+
+//   console.log(persone.name);
+
+//   let answer = confirm('Are you here?');
+
+//   console.log(answer);
+
+let incr = 10,
+    decr = 10;
+
